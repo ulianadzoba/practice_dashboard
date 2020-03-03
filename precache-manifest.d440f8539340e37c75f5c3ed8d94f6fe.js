5e75009be3841afc4ee234b3e694211c7221c8ac
Uliana Dzoba <ulianadzoba@gmail.com>
2020-03-03 08:42:29 +0200

    Updates

---
+++ precache-manifest.d440f8539340e37c75f5c3ed8d94f6fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a354def5dafb4a1bfbdfccdb65c0002",
+    "url": "/practice_dashboard/index.html"
+  },
+  {
+    "revision": "83580c82f5ff60572da3",
+    "url": "/practice_dashboard/static/css/main.97febf11.chunk.css"
+  },
+  {
+    "revision": "578c8cdd35157f51b854",
+    "url": "/practice_dashboard/static/js/2.5445c443.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/practice_dashboard/static/js/2.5445c443.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83580c82f5ff60572da3",
+    "url": "/practice_dashboard/static/js/main.08c0b31f.chunk.js"
+  },
+  {
+    "revision": "58dbfa40519df8cf72cf",
+    "url": "/practice_dashboard/static/js/runtime-main.ae3acd4b.js"
+  }
+]);
